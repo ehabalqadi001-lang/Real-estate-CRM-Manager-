3e1e2371d3d441ce117573c36291db76d29bb768
Ehab Alqadi <ehab.alqadi.001@gmail.com>
2026-04-13 00:13:07 +0200

    Victory: Clean build successful, all i18n remnants removed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,10 @@
 // استدعاء المكون في الأعلى
-import LanguageSwitcher from '@/components/LanguageSwitcher';
+
 
 // ... داخل كود الـ Navbar الخاص بك، في منطقة الأزرار (أقصى اليسار في العربي):
 <div className="flex items-center gap-4">
   
   {/* زر تبديل اللغة */}
-  <LanguageSwitcher />
+  
   
 </div>
